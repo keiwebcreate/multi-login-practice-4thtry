c5a5ad48da6df9ef981303492cab283faf707c93
IMHUNGRYINCHEM\imhun <keicreate20240704@gmail.com>
2026-07-03 13:01:17 +0900

    Laravel Breezeを有効化し、webpackからviteに変更しました

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Blade;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -23,6 +24,18 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        Blade::directive('vite', function ($expression) {
+            return "<?php 
+                if (file_exists(public_path('hot'))) {
+                    echo '<script type=\"module\" src=\"http://localhost:5173/@vite/client\"></script>';
+                    echo '<script type=\"module\" src=\"http://localhost:5173/resources/js/app.js\"></script>';
+                    echo '<link rel=\"stylesheet\" href=\"http://localhost:5173/resources/css/app.css\">';
+                } else {
+                    \$manifest = json_decode(file_get_contents(public_path('build/manifest.json')), true);
+                    echo '<link rel=\"stylesheet\" href=\"' . asset('build/' . \$manifest['resources/css/app.css']['file']) . '\">';
+                    echo '<script type=\"module\" src=\"' . asset('build/' . \$manifest['resources/js/app.js']['file']) . '\"></script>';
+                }
+            ?>";
+        });
     }
 }
