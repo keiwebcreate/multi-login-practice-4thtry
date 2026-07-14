b87d0540ff12b4b46454ec21cb13f503be47acc6
IMHUNGRYINCHEM\imhun <keicreate20240704@gmail.com>
2026-07-14 19:58:09 +0900

    Shopコントローラと使用するビューファイルを作成しました。

--- database/migrations/2026_07_14_181040_create_shops_table.php
+++ database/migrations/2026_07_14_181040_create_shops_table.php
@@ -15,7 +15,7 @@ class CreateShopsTable extends Migration
     {
         Schema::create('shops', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('owner_id')->constrained()->onDelete('cascade');
+            $table->foreignId('owner_id')->constrained()->OnUpdate('cascade')->onDelete('cascade');
             $table->string('name');
             $table->text('information');
             $table->string('filename');
--- routes/owner.php
+++ routes/owner.php
@@ -10,6 +10,7 @@ use App\Http\Controllers\Owner\Auth\NewPasswordController;
 use App\Http\Controllers\Owner\Auth\PasswordResetLinkController;
 use App\Http\Controllers\Owner\Auth\RegisteredUserController;
 use App\Http\Controllers\Owner\Auth\VerifyEmailController;
+use App\Http\Controllers\Owner\ShopsController;
 
 
 /*
@@ -25,6 +26,12 @@ use App\Http\Controllers\Owner\Auth\VerifyEmailController;
 
 Route::get('/', function () {
     return view('owner.welcome');
+});
+
+Route::prefix('/shops')->middleware(['auth:owners'])->group(function () {
+  Route::get('index',[ShopsController::class, 'index'])->name('shops.index');
+  Route::get('edit/{shop}',[ShopsController::class, 'edit'])->name('shops.edit');
+  Route::post('update/{shop}',[ShopsController::class, 'update'])->name('shops.update');
 });
 
 Route::get('/dashboard', function () {
